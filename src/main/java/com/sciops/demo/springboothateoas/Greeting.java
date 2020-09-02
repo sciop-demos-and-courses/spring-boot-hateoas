dd2883454f89bb3ce493c68e0aa10a16a0e70db8
sciops <sciops@work-P6689-MD61021>
2020-09-02 18:27:36 +0200

    Done

---
+++ src/main/java/com/sciops/demo/springboothateoas/Greeting.java
@@ -0,0 +1,19 @@
+package com.sciops.demo.springboothateoas;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting extends RepresentationModel<Greeting> {
+	private final String content;
+
+	@JsonCreator
+	public Greeting(@JsonProperty("content") String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
