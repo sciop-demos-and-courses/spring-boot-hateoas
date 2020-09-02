8a99a17c409e6e8ab8b4a4b00a3b846ce0ca5385
sciops <sciops@work-P6689-MD61021>
2020-09-02 17:54:30 +0200

    initial commit

---
+++ src/main/java/com/sciops/demo/springboothateoas/SpringBootHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.sciops.demo.springboothateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHateoasApplication.class, args);
+	}
+
+}
